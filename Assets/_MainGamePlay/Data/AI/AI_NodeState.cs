9d01ef59d0c7edee8fc8a37a992882418ea3fb67
Jeff Simon <jeffsimab@gmail.com>
2023-12-24 17:20:20 -0800

    asdf

---
+++ Assets/_MainGamePlay/Data/AI/AI_NodeState.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+
+public enum AIActionType { SendWorkersToNode, ConstructBuildingInOwnedNode };
+
+public class AIAction
+{
+    public AIActionType Type;
+    public int Count;
+    public NodeData SourceNode;
+    public NodeData DestNode;
+}
+
+class AI_PlayerState
+{
+
+}
+
+class AI_BuildingState
+{
+    public BuildingDefn buildingDefn;
+
+    public AI_BuildingState(BuildingData building)
+    {
+        buildingDefn = building?.Defn;
+    }
+}
+
+class AI_NodeState
+{
+    private NodeData nodeData;
+    public List<AI_NodeState> NeighborNodes;
+    public int NumWorkers;
+
+    public AI_BuildingState Building;
+    public bool HasBuilding => Building.buildingDefn != null;
+    public PlayerData OwnedBy;
+
+    public AI_NodeState(NodeData nodeData)
+    {
+        this.nodeData = nodeData;
+        Building = new AI_BuildingState(nodeData.Building);
+        NumWorkers = nodeData.NumWorkers;
+        OwnedBy = nodeData.OwnedBy;
+    }
+}
