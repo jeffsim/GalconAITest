bce90dcd18c6a144afc0c1eeb4fa73eebbb5802d
Jeff Simon <jeffsimab@gmail.com>
2023-12-25 04:13:31 -0800

    asd

--- Assets/_MainGamePlay/Defns/BuildingDefn.cs
+++ Assets/_MainGamePlay/Defns/BuildingDefn.cs
@@ -1,4 +1,3 @@
-
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -9,5 +8,22 @@ public class BuildingDefn : BaseDefn
     public string Name;
     public Color Color = Color.white;
     public WorkerDefn WorkerDefn;
+
+    // Construction
+    [Header("Construction"), Space(10)]
+    public bool CanBeBuiltByPlayer = true;
+    [ShowIf("CanBeBuiltByPlayer")]
     public List<Good_CraftingRequirements> ConstructionRequirements = new();
+
+    // Gathering
+    [Header("Gathering"), Space(10)]
+    public bool CanGatherResources = false;
+    [ShowIf("CanGatherResources")]
+    public GoodDefn GatherableResource;
+
+    // Crafting
+    [Header("Crafting"), Space(10)]
+    public bool CanCraftGoods = false;
+    [ShowIf("CanCraftGoods")]
+    public List<GoodDefn> CraftableGoods = new();
 }
--- Assets/_MainGamePlay/Defns/GoodDefn.cs
+++ Assets/_MainGamePlay/Defns/GoodDefn.cs
@@ -2,18 +2,18 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Good
-{
-    // Raw Resources
-    Wood, Stone, GoldOre, IronOre, Flour, Water,
+// public enum Good
+// {
+//     // Raw Resources
+//     Wood, Stone, GoldOre, IronOre, Flour, Water,
 
-    // Crafted Resources
-    GoldBar, IronBar,
-    Planks, Bricks, Coins, Tools, Weapons,
+//     // Crafted Resources
+//     GoldBar, IronBar,
+//     Planks, Bricks, Coins, Tools, Weapons,
 
-    // Food
-    Fish, Meat, Bread, Beer
-};
+//     // Food
+//     Fish, Meat, Bread, Beer
+// };
 
 [Serializable]
 public class Good_CraftingRequirements
@@ -27,6 +27,5 @@ public class GoodDefn : BaseDefn
 {
     public string FriendlyName;
     public List<Good_CraftingRequirements> CraftingRequirements = new List<Good_CraftingRequirements>();
-    public Sprite SpriteToShowInTimer;
-    public Sprite SpriteToShowInBuildingInfo;
+    public Sprite Sprite;
 }
