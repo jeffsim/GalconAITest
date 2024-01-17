9860893cf34e8fa8298bf19a7135e18325f44a5d
Jeff Simon <jeffsimab@gmail.com>
2024-01-16 16:23:34 -0800

    a

--- Assets/_MainGamePlay/Scene/AITestScene/UI/AIDebugger/AIDebuggerEntry.cs
+++ Assets/_MainGamePlay/Scene/AITestScene/UI/AIDebugger/AIDebuggerEntry.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 public class AIDebuggerEntry : MonoBehaviour
 {
     public TextMeshProUGUI ActionNumber;
-    public TextMeshProUGUI ActionType;
     public TextMeshProUGUI Information;
     public TextMeshProUGUI Score;
     public TextMeshProUGUI Count;
@@ -21,22 +20,12 @@ public class AIDebuggerEntry : MonoBehaviour
         switch (entry.ActionType)
         {
             case AIActionType.AttackFromNode:
-                ActionType.text = "Attack";
                 break;
-            // case AIActionType.ConstructBuildingInOwnedEmptyNode:
-            //     ActionType.text = "Construct";
-            //     break;
             case AIActionType.ConstructBuildingInEmptyNode:
-                ActionType.text = "Construct";
                 break;
             case AIActionType.SendWorkersToOwnedNode:
-                ActionType.text = "Send";
                 break;
-            // case AIActionType.SendWorkersToEmptyNode:
-            //     ActionType.text = "Send";
-            //     break;
             default:
-                ActionType.text = "TODO: " + entry.ActionType + "";
                 break;
         }
         Information.text = entry.InformationString();
