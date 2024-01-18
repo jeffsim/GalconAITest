9884337e0341d3f4900d47d6452c55aaea1db567
Jeff Simon <jeffsimab@gmail.com>
2024-01-18 10:32:50 -0800

    Update TrySendWorkersToOwnedNode.cs

--- Assets/_MainGamePlay/Data/AI/AIActions/TrySendWorkersToOwnedNode.cs
+++ Assets/_MainGamePlay/Data/AI/AIActions/TrySendWorkersToOwnedNode.cs
@@ -22,34 +22,35 @@ public partial class PlayerAI
         for (int i = 0; i < count; i++)
         {
             var toNode = fromNode.NeighborNodes[i];
-            
-        //             // Verify we can perform the action
-        //             if (toNode.OwnedBy != player) continue; // This task can only send workers to nodes owned by this player
 
-        //             // Perform the action and get the score of the state after the action is performed
-        //             aiTownState.SendWorkersToOwnedNode(fromNode, toNode, .5f, out int numSent);
-        //             aiTownState.EvaluateScore(curDepth, maxDepth, out float scoreAfterActionAndBeforeSubActions, out DebugAIStateReasons debugOutput_actionScoreReasons);
+            //             // Verify we can perform the action
+            //             if (toNode.OwnedBy != player) continue; // This task can only send workers to nodes owned by this player
 
-        // #if DEBUG
-        //             AIDebugger.TrackPerformAction_SendWorkersToOwnedNode(toNode, numSent, scoreAfterActionAndBeforeSubActions);
-        // #endif
-        //             // Recursively determine what the best action is after this action is performed
-        //             var actionScore = RecursivelyDetermineBestAction(curDepth + 1, scoreAfterActionAndBeforeSubActions);
-        //             if (actionScore.Score > bestAction.Score)
-        //             {
-        //                 // This is the best action so far in this 'level' of the AI stack; save the action so we can return it
-        //                 bestAction.Score = actionScore.ScoreBeforeSubActions;
-        //                 bestAction.Type = AIActionType.SendWorkersToOwnedNode;
-        //                 bestAction.Count = numSent;
-        //                 bestAction.SourceNode = fromNode;
-        //                 bestAction.DestNode = toNode;
-        // #if DEBUG
-        //                 bestAction.TrackStrategyDebugInfoInAction(actionScore, debugOutput_actionScoreReasons, thisActionNum, recurseCount, curDepth);
-        // #endif
-        //             }
+            //             // Perform the action and get the score of the state after the action is performed
+            //             aiTownState.SendWorkersToOwnedNode(fromNode, toNode, .5f, out int numSent);
+            //             aiTownState.EvaluateScore(curDepth, maxDepth, out float scoreAfterActionAndBeforeSubActions, out DebugAIStateReasons debugOutput_actionScoreReasons);
 
-        //             // Undo the action
-        //             aiTownState.Undo_SendWorkersToOwnedNode(fromNode, toNode, numSent);
-        //         }
+            // #if DEBUG
+            //             AIDebugger.TrackPerformAction_SendWorkersToOwnedNode(toNode, numSent, scoreAfterActionAndBeforeSubActions);
+            // #endif
+            //             // Recursively determine what the best action is after this action is performed
+            //             var actionScore = RecursivelyDetermineBestAction(curDepth + 1, scoreAfterActionAndBeforeSubActions);
+            //             if (actionScore.Score > bestAction.Score)
+            //             {
+            //                 // This is the best action so far in this 'level' of the AI stack; save the action so we can return it
+            //                 bestAction.Score = actionScore.ScoreBeforeSubActions;
+            //                 bestAction.Type = AIActionType.SendWorkersToOwnedNode;
+            //                 bestAction.Count = numSent;
+            //                 bestAction.SourceNode = fromNode;
+            //                 bestAction.DestNode = toNode;
+            // #if DEBUG
+            //                 bestAction.TrackStrategyDebugInfoInAction(actionScore, debugOutput_actionScoreReasons, thisActionNum, recurseCount, curDepth);
+            // #endif
+            //             }
+
+            //             // Undo the action
+            //             aiTownState.Undo_SendWorkersToOwnedNode(fromNode, toNode, numSent);
+            //         }
+        }
     }
 }
